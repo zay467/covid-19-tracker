82174fc2991d546ab464f62a14f77935e8689c20
Zay Min Maw <zayminmaw77@gmail.com>
2021-04-28 11:13:56 +0630

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import "./App.css";
+import InfoBox from "./components/InfoBox";
+import Map from "./components/Map";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>Covid-19 TRACKER</h1>
+        <FormControl className="app__dropdrown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats">
+        <InfoBox cases="400" total="2000" title="Coronavirus Cases" />
+        <InfoBox cases="200" total="3000" title="Recovered" />
+        <InfoBox cases="123" total="1000" title="Deaths" />
+      </div>
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,9 @@
+const Map = () => {
+  return (
+    <div className="map">
+      <h1>I am a map</h1>
+    </div>
+  );
+};
+
+export default Map;
